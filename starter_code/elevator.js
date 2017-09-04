83982f24a34dfa21286daa76a0d8a97e403d6b55
raquel500c <raquel500c@gmail.com>
2017-09-04 19:03:28 +0200

    iteration2 done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,52 @@
+/*jshint esversion:6*/
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = 'up';
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    setInterval(() => this.update(), 1000);
+  }
+
+  stop() {
+    clearInterval(() => this.start());
+  }
+
+  update() {
+    this.log();
+  }
+
+  _passengersEnter() {
+
+  }
+
+  _passengersLeave() {
+
+  }
+
+  floorUp() {
+    if (this.floor < this.MAXFLOOR) {
+      this.floor++;
+      this.direction = 'up';
+    }
+  }
+
+  floorDown() {
+    if (this.floor > 0) {
+      this.floor--;
+      this.direction = 'down';
+    }
+  }
+
+
+  call() {}
+
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,14 @@
+/*jshint esversion:6*/
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+let elevator= new Elevator();
+elevator.update();
+elevator.floorDown();
+elevator.update();
+//elevator.start();
+//elevator.stop();
+elevator.floorUp();
+elevator.update();
+elevator.floorDown();
+elevator.update();
